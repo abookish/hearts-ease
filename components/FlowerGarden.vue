aa67d338f44ea9b0752867c18aadcb501f81a959
abookish <snsteinca@gmail.com>
2024-02-10 10:49:07 -0800

    examples

--- components/FlowerGarden.vue
+++ components/FlowerGarden.vue
@@ -1,6 +1,27 @@
 <script setup>
-import * as fetchapimethods from fetchapimethods
-const uri = fetchapimethods.getURI()
+import {getURI} from 'fetchapimethods'
 const { data, error, pending, refresh } = await useFetch('/api/todos')
+/*<script setup>
+const { data, pending, error, refresh } = await useFetch('/api/modules');
 
+if (pending.value) {
+  // Show loading indicator
+}
+
+if (error.value) {
+  // Handle error
+  console.error('Error fetching data:', error.value);
+}
+
+function handleRefresh() {
+  // Manually trigger data refresh
+  refresh();
+}
+</script>
+
+
+const param1 = ref('value1')
+const { data, pending, error, refresh } = await useFetch('/api/modules', {
+  query: { param1, param2: 'value2' }
+})*/
 </script>
